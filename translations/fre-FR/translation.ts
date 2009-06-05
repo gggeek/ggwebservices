079e2dd439836f61e22dcb7b195d960b59b829c6
gggeek <gg@ez.no>
2009-06-05 10:39:51 +0000

    - add translations

---
+++ translations/fre-FR/translation.ts
@@ -0,0 +1,25 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>kernel/navigationpart</name>
+    <message>
+        <source>Webservices Debugger</source>
+        <comment>Navigation part</comment>
+        <translation>Debogueur Webservices</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ggwebservices</name>
+    <message>
+        <source>WS Debugger</source>
+        <translation>Debogueu WS</translation>
+    </message>
+    <message>
+        <source>Local server</source>
+        <translation>Serveur local</translation>
+    </message>
+    <message>
+        <source>Remote servers</source>
+        <translation>Serveurs distants</translation>
+    </message>
+</context>
+</TS>
---
+++ translations/ita-IT/translation.ts
@@ -0,0 +1,25 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>kernel/navigationpart</name>
+    <message>
+        <source>Webservices Debugger</source>
+        <comment>Navigation part</comment>
+        <translation>Debugger di webservices</translation>
+    </message>
+</context>
+<context>
+    <name>extension/ggwebservices</name>
+    <message>
+        <source>WS Debugger</source>
+        <translation>Debugger WS</translation>
+    </message>
+    <message>
+        <source>Local server</source>
+        <translation>Server locale</translation>
+    </message>
+    <message>
+        <source>Remote servers</source>
+        <translation>Server remoti</translation>
+    </message>
+</context>
+</TS>
