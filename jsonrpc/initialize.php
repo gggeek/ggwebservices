941d5099344a10cf0f0fa2aa7ed7c61753eecc6e
gggeek <gg@ez.no>
2009-06-24 11:03:19 +0000

    - doc updates

--- jsonrpc/initialize.php
+++ jsonrpc/initialize.php
@@ -4,7 +4,7 @@
  */
 
 /*
-$server->registerFunction( 'fetchSyndicationFeedObjectList', // name of exposed webservice
+$server->registerFunction( 'fetchSyndicationFeedObjectList', // name of exposed webservice AND php function at the same time
                            array( 'feedID' => 'integer' ), // input params array. Keys are not really used, as param validation is positional. Use null instead of an array to avoid type validation
                            'array', // type of return value
                            'Returns a list of...' );
@@ -13,11 +13,13 @@ function fetchSyndicationFeedObjectList( $feedID )
 {
     if ( $feedID <= 0 )
     {
-         // return a protocol-level error
-         return ggWebservicesFault( -1, 'Invalid feed ID: negative value' );
-     }
-     else
-         return array ( 'a', 'b', 'c' );
+        // return a protocol-level error
+        return new ggWebservicesFault( -1, 'Invalid feed ID: negative value' );
+    }
+    else
+    {
+        return array( 'a', 'b', 'c' );
+    }
 }
 */
 
