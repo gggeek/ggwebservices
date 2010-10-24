c7e4c609e32755706395db3e86efec8dff063608
gggeek <gg@ez.no>
2010-10-24 23:23:37 +0000

    - cleanup leftover comments

--- modules/webservices/debugger/controller.php
+++ modules/webservices/debugger/controller.php
@@ -12,14 +12,8 @@ include( dirname( __FILE__ ) . "/common.php" );
 if ( $params['action'] == '' )
     $params['action'] = 'list';
 
-/*// pass on control to actual debugger, setting some vars for it
-$editorpath = '/extension/ggwebservices/design/';
-eZURI::transformURI( $editorpath, true, 'full' );
-$visualeditorpath = '../visualeditor';*/
-
 require_once( "kernel/common/template.php" );
 $tpl = templateInit();
-/// @todo set vars to tpl
 $tpl->setVariable( 'params', $params );
 $Result['content'] = $tpl->fetch( "design:webservices/debugger/controller.tpl" );
 $Result['pagelayout'] = 'debugger_pagelayout.tpl';
