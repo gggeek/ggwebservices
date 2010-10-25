ad3259bd07946744f55e07954ac98c0b7d6aa04f
gggeek <gg@ez.no>
2010-10-25 00:11:10 +0000

    - update declaration of included sw in ezinfo.php

--- ezinfo.php
+++ ezinfo.php
@@ -10,12 +10,24 @@ class ggwebservicesInfo
             'Copyright' => "Copyright (C) 2009-2010 Gaetano Giunta",
             'License' => "GNU General Public License v2.0",
             'Includes the following third-party software' => array(
-                'phpxmlrpc' => 'http://phpxmlrpc.sourceforge.net/',
+                'Name' => 'YUI',
+                'Version' => "2.5.0",
+                'Copyright' => 'Copyright (c) 2010, Yahoo! Inc. All rights reserved.',
+                'License' => 'Licensed under the BSD License' ),
+            'Includes the following third-party software (2)' => array(
+                'Name' => 'phpxmlrpc',
+                'Version' => "3.0.0.beta",
+                'Copyright' => 'Copyright (c) 1999,2000,2002 Edd Dumbill.',
+                'License' => 'Licensed under the BSD License' ),
+            'Includes the following third-party software (3)' => array(
+                'Name' => 'jQuery JSON Plugin',
+                'Version' => "2.1",
+                'Copyright' => 'Brantley Harris (?)',
+                'License' => 'MIT License' )
                 /*'nuSOAP' => array(
                     'Version' => '2008-04-06',
                     'License' => 'GNU/LGPL  v2.1 - Copyright (c) 2002 NuSphere Corporation',
                     'For more information' => 'http://sourceforge.net/projects/nusoap' ),*/
-               )
         );
     }
 }
