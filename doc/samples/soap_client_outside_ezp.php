c081d20dd709f021e0daee4ceea1101c0aed102f
gggeek <gg@ez.no>
2010-12-18 01:31:09 +0000

    - tag for release

--- doc/samples/soap_client_outside_ezp.php
+++ doc/samples/soap_client_outside_ezp.php
@@ -1,3 +1,4 @@
+<pre>
 <?php
 /**
  * Sample soap client that uses the ggws classes outside of an eZ Publish context
@@ -12,10 +13,13 @@
 // include client classes (this is done by autload when within an eZP context)
 include_once( "ggwebservices/classes/ggwebservicesclient.php" );
 include_once( "ggwebservices/classes/ggphpsoapclient.php" );
+include_once( "ggwebservices/classes/ggphpsoapclienttransport.php" );
 include_once( "ggwebservices/classes/ggwebservicesrequest.php" );
+include_once( "ggwebservices/classes/ggsoaprequest.php" );
 include_once( "ggwebservices/classes/ggphpsoaprequest.php" );
 include_once( "ggwebservices/classes/ggwebservicesresponse.php" );
 include_once( "ggwebservices/classes/ggphpsoapresponse.php" );
+include_once( "ggwebservices/classes/ggwebservicesfault.php" );
 
 // create a new client
 $client = new ggPhpSOAPClient( "mssoapinterop.org", "/asmx/simple.asmx", 80, null, 'http://mssoapinterop.org/asmx/simple.asmx?WSDL' );
@@ -25,17 +29,24 @@ $client = new ggPhpSOAPClient( "mssoapinterop.org", "/asmx/simple.asmx", 80, nul
 // define the request
 $request = new ggPhpSOAPRequest( "echoInteger", array( 123 ) );
 
+// set client to debug mode
+$client->setOption( 'debug', 2 );
+
 // send the request to the server and fetch the response
 $response = $client->send( $request );
+
+// show debug info
+echo 'SENT:  <div style="background-color: silver">' . htmlspecialchars( $client->RequestPayload() ) . "</div>";
+echo 'RECEIVED: <div style="background-color: silver">' . htmlspecialchars( $client->ResponsePayload() ) . "</div>";
 
 // check if the server returned a fault, if not print out the result
 if ( $response->isFault() )
 {
-    print( "<pre>Fault: " . $response->faultCode(). " - \"" . $response->faultString() . "\"" );
+    print( "Fault: " . $response->faultCode(). " - \"" . $response->faultString() . "\"" );
 }
 else
 {
-    print( "<pre>Returned value was: \"" . $response->value() . "\"" );
+    print( "Returned value was: \"" . $response->value() . "\"" );
 }
 
 ?>
