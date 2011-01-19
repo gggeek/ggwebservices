e0b75710434f9d3d7988e593258be9ebbcded2ac
gggeek <gg@ez.no>
2011-01-19 22:43:00 +0000

    - improve comments in ini file

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -6,11 +6,8 @@ ExtensionAutoloadPath[]=ggwebservices
 [RegionalSettings]
 TranslationExtensions[]=ggwebservices
 
-# To allow calls to the exposed webservices
-# from external (non-ajax) webservice clients
-# that cannot use a session cookie,
-# uncomment the folllwoing line:
 [RoleSettings]
+# The following view does permissions checking on its own, based on the executed webservice
 PolicyOmitList[]=webservices/execute
 
 */ ?>
