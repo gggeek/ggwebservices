1cb072cbed2a7b542c8f5fc2b0bbf602088c2b33
gggeek <gg@ez.no>
2011-04-20 13:43:09 +0000

    - fix: declaration of custom cache was incomplete

--- settings/site.ini.append.php
+++ settings/site.ini.append.php
@@ -12,6 +12,10 @@ PolicyOmitList[]=webservices/execute
 PolicyOmitList[]=webservices/wsdl
 PolicyOmitList[]=webservices/xsd
 
+# Cache item entry (for eZ Publish 4.3 and up)
+[Cache]
+CacheItems[]=webservices
+
 [Cache_webservices]
 name=ggWebservices server-side wsdl cache
 path=webservices
